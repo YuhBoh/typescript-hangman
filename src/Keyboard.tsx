5806e7e343e0e35454c73dc0759aa52b14fc6af6
Yuh-Boh Feng <fengyuhboh@gmail.com>
2023-07-02 17:37:49 -0700

    keyboard is added

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Keys from './Keys.json';
+import "./Keyboard.css";
 
 export function Keyboard() {
   return <div 
     style={{ 
       display: "grid", 
-      gridTemplateColumns: "repeat(auto-fit, minmix(75px, 1fr)",
+      gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
       gap: ".5rem",
     }}>
       {Keys.map(key => {
-        return <button key={key}>{key}</button>
-
+        return <button className="btn" key={key}>{key}</button>
       })}
     </div>
 }
